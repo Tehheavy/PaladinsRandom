e6cbd78bd66885d761c85d52ade818dd2e349b49
Alex Dushevski <39350701+Tehheavy@users.noreply.github.com>
2021-03-20 20:49:40 +0200

    sergay angry

--- functions.js
+++ functions.js
@@ -83,7 +83,7 @@ function shuffleArray(array) {
 function scramble() {
     users = shuffleArray(users);
     for (let i = 0; i < users.length; i++) {
-        let champid = (Math.floor(Math.random() * 46) + 1).toString();
+        let champid = (Math.floor(Math.random() * 47) + 1).toString();
         console.log("for user:" + users[i].username + " randomed:" + champid);
         let legendaryid = Math.floor(Math.random() * 3) + 1;
         users[i].champion = champid;
